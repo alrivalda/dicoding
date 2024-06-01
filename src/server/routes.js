ff8f5bc6105501e5eba8bb1a9cb4ea876ba83806
muhammad.al15 <muhammad.al15@ui.ac.id>
2024-06-01 23:54:06 +0700

    fix

--- src/server/routes.js
+++ src/server/routes.js
@@ -10,6 +10,12 @@ const routes = [
         allow: "multipart/form-data",
         multipart: true,
         maxBytes: 1000000,
+        failAction: async (request, h, err) => {
+          throw Boom.tooLarge(
+            "Payload content length greater than maximum allowed: 1000000",
+            [err]
+          );
+        },
       },
     },
   },
